506ba054d7bf4f0f99a52f7c2ef5ff98f92e199c
Elnaz <elnazmalakouti@gmail.com>
2022-11-23 23:36:57 +0330

    Add Button Component

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,7 +1,7 @@
-const Button = (props) => {
+const Button = ({children, ...props}) => {
     return(
         <button className="w-[300px] h-[60px] bg-[#9E1010] text-white rounded-[.25rem] font-[vazirbold]">
-            {props.children}
+            {children}
         </button>
     )
 }
