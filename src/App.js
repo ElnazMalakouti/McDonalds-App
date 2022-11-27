609c197ff1701e8919cb7e370803a63c075fb222
Elnaz <elnazmalakouti@gmail.com>
2022-11-27 17:23:53 +0330

    Add Discount Validation  message

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
     <div className="App">
       <FoodsMenu/>      
       <OrderCard/>
+
+      {/* <div className='w-full h-full bg-black sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-white'>test</div> */}
     </div>
   );
 }
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,6 +1,6 @@
 import { MdDone } from "react-icons/md"
 import Button from '../Button'
-import {useDispatch , useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import HyperModal from "react-hyper-modal"
 import { submitOrder } from "../../redux/reducers/orderReducer/orderReducer"
@@ -16,6 +16,8 @@ const OrderCard = () => {
 
     const [totalPrice, setTotalPrice] = useState(0)
     const [discount, setDiscount] = useState({ discountCode: "", percent: 0 })
+    const [discountError, setDiscountError] = useState("")
+    const [discountValid , setDiscountValid] = useState("")
 
     useEffect(() => {
         setTotalPrice(
@@ -53,17 +55,30 @@ const OrderCard = () => {
                 ...discount,
                 percent: (temp.percent * totalPrice) / 100
             })
+            setDiscountValid("کد تخفیف ثبت شد")
+            setDiscountError("")
+        } else {
+            setDiscount({
+                ...discount,
+                percent: 0
+            })
+            setDiscountError("کد تخفیف وارد شده نامعتبر است!")
+            setDiscountValid("")
         }
     }
 
+    useEffect(()=>{
+        setDiscountError("")
+    },[discount.discountCode])
+
     return (
         <>
-            <HyperModal isOpen={isModalOpen} requestClose={()=> setIsModalOpen(false)}>
+            <HyperModal isOpen={isModalOpen} requestClose={() => setIsModalOpen(false)}>
                 <div className="w-full h-full flex flex-col justify-between items-center pt-[3rem] pb-[2rem]">
                     <p className="font-[vazirbold]">سفارش شما با موفقیت ثبت شد.</p>
                     <div className="w-[40%]">
-                        <Button onClick={()=> setIsModalOpen(false)}>بستن</Button>
-                    </div> 
+                        <Button onClick={() => setIsModalOpen(false)}>بستن</Button>
+                    </div>
                 </div>
             </HyperModal>
             <div className="w-[27%] h-[45%] p-5 bg-[#F0F8FF] rounded-[1rem] font-[vazirdig] text-black flex flex-col">
@@ -88,21 +103,25 @@ const OrderCard = () => {
                     <div className="w-full flex flex-row justify-between">
                         <p>تخفیف:</p>
                         <p>
-                            {(totalPrice)>0 ? (discount.percent) : 0}
+                            {(totalPrice) > 0 ? (discount.percent) : 0}
                             <span> تومان</span>
                         </p>
                     </div>
                 </div>
 
-                <div className="w-full flex flex-row justify-center items-center mt-2">
-                    <input value={discount.discountCode} onChange={e => setDiscount({ ...discount, discountCode: e.target.value })} className="w-[88%] h-[2.5rem] p-2 text-[15px] outline-none" placeholder="کد تخفیف" />
-                    <button onClick={discountCalculator} className="w-[12%] h-[2.5rem] bg-[#9E1010] rounded-tl-[.25rem] rounded-bl-[.25rem] text-white flex justify-center items-center text-[22px]"><><MdDone /></></button>
+                <div className="w-full flex flex-col gap-2">
+                    <div className="w-full flex flex-row justify-center items-center mt-2">
+                        <input value={discount.discountCode} onChange={e => setDiscount({ ...discount, discountCode: e.target.value })} className="w-[88%] h-[2.5rem] p-2 text-[15px] outline-none" placeholder="کد تخفیف" />
+                        <button onClick={discountCalculator} className="w-[12%] h-[2.5rem] bg-[#9E1010] rounded-tl-[.25rem] rounded-bl-[.25rem] text-white flex justify-center items-center text-[22px]"><><MdDone /></></button>
+                    </div>
+                    <small className="text-red-500 text-[12px]">{discountError}</small>
+                    <small className="text-green-500 text-[12px]">{discountValid}</small>
                 </div>
 
                 <div className="w-full flex flex-row justify-between bg-[#FFC72C] mt-[3rem] p-[.3rem] font-[vazirdig] font-bold">
                     <p>مبلغ قابل پرداخت:</p>
                     <p>
-                        {((totalPrice + ((9 * totalPrice) / 100)) - discount.percent)>0 ? ((totalPrice + ((9 * totalPrice) / 100)) - discount.percent) : 0}
+                        {((totalPrice + ((9 * totalPrice) / 100)) - discount.percent) > 0 ? ((totalPrice + ((9 * totalPrice) / 100)) - discount.percent) : 0}
                         <span> تومان</span>
                     </p>
                 </div>
@@ -110,9 +129,9 @@ const OrderCard = () => {
                 <div className="w-full mt-4">
                     <Button disabled={orderlist.length === 0 ? true : false} onClick={() => {
                         dispatch(submitOrder())
-                        setDiscount({...discount, discountCode: ""})                        
+                        setDiscount({ ...discount, discountCode: "" })
                         setIsModalOpen(true)
-                        }}>
+                    }}>
                         ثبت سفارش
                     </Button>
                 </div>
